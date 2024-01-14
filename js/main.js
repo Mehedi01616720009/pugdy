398004e1751d1bbe39fa1c776844d20c26520058
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-01-14 12:26:21 +0600

    join us

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
 })
 
+// copy telegram link
+const copyBtn = document.getElementById('copy-telegram-btn'),
+    copyLink = document.getElementById('copy-telegram-link'),
+    copyTooltip = document.getElementById('copy-telegram-tooltip');
+
+    copyBtn.addEventListener('click', async () => {
+        try {
+            await navigator.clipboard.writeText(copyLink.innerText);
+
+            copyTooltip.innerText = 'Copied!';
+            copyTooltip.style.display = 'block';
+
+            // Hide tooltip after 3 seconds
+            setTimeout(() => {
+                copyTooltip.style.display = 'none';
+            }, 3000);
+        } catch (err) {
+            console.error('Unable to copy text to clipboard', err);
+        }
+    })
+
 // anchorScroll
 var anchorScroll = $(document).AnchorScroll({
     speed: 1000

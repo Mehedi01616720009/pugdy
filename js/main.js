212d4fffceab2ee457079d8de8b5163f098c8bae
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-07-11 19:49:33 +0600

    new bg anime

--- js/main.js
+++ js/main.js
@@ -1,40 +1,19 @@
 // mobile menu toggle
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
-navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-})
+navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+});
 
-navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-})
-
-// copy telegram link
-const copyBtn = document.getElementById('copy-telegram-btn'),
-    copyLink = document.getElementById('copy-telegram-link'),
-    copyTooltip = document.getElementById('copy-telegram-tooltip');
-
-    copyBtn.addEventListener('click', async () => {
-        try {
-            await navigator.clipboard.writeText(copyLink.innerText);
-
-            copyTooltip.innerText = 'Copied!';
-            copyTooltip.style.display = 'block';
-
-            // Hide tooltip after 3 seconds
-            setTimeout(() => {
-                copyTooltip.style.display = 'none';
-            }, 2000);
-        } catch (err) {
-            console.error('Unable to copy text to clipboard', err);
-        }
-    })
+navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+});
 
 // anchorScroll
 var anchorScroll = $(document).AnchorScroll({
-    speed: 1000
+    speed: 1000,
 });
 
 // wowJs
